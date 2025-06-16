84aad1d45cb16233c94158fe2e1efd7d1be90beb
Jerryfel13 <fellaraga@gmail.com>
2025-06-16 17:30:53 +0800

    updated version

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <footer className="w-full py-4 text-center text-xs text-gray-500 border-t bg-white mt-auto">
-      Copyright &copy; 2025 &mdash; v1.0.0
+      Copyright &copy; 2025 &mdash; v1.0.1
     </footer>
   );
 } 
